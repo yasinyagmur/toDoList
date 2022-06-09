3761156ad9271280b4c1ba3f33926948ccf55fd0
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-10 00:08:31 +0300

    js file was edited

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-const input = document.querySelector('.user--input');
-const add = document.querySelector('.add');
-const task = document.querySelector('.toDoApp');
+const input = document.querySelector(".user--input");
+const add = document.querySelector(".add");
+const task = document.querySelector(".toDoApp");
 
+const mission = () => {
+  if (input.value === "") {
+    alert("please enter");
+  } else {
+    const appendli = task.appendChild(document.createElement("li"));
+    appendli.innerHTML = input.value.charAt(0).toUpperCase() + input.value;
 
+    const done = appendli.appendChild(document.createElement("i"));
+    // console.log(done)
+    const trash = appendli.appendChild(document.createElement("i"));
+    // console.log(trash)
 
-const mission = ()=>{
-    if(input.value === ''){
-        alert('please enter')
+    appendli.className = "appendli";
+    done.className = "fas fa-check";
+    trash.className = "fas fa-trash-alt";
+    input.value = "";
+
+    trash.addEventListener("click", trashli);
+    done.addEventListener("click", doneli);
+    function doneli(e) {
+      appendli.style.textDecoration = "line-through";
+      input.focus();
     }
-    else{
-        const appendli = task.appendChild(document.createElement("li"));
-        appendli.innerHTML = input.value.charAt(0).toUpperCase() + input.value
-
-        const done = appendli.appendChild(document.createElement("i"));
-        // console.log(done)
-        const trash = appendli.appendChild(document.createElement("i"));
-        // console.log(trash)
-        
-        appendli.className = "appendli";
-        
-        done.className = "fas fa-check";
-        trash.className = "fas fa-trash-alt";
-        input.value=''
-
-        trash.addEventListener("click", trashli);
-        done.addEventListener("click", doneli);
-        function doneli(e) {
-            e.target.previousSibling.style.textDecoration = "line-through"
-        }
-        function trashli(e) {
-            e.target.parentElement.remove()
-        }
-
+    function trashli(e) {
+      e.target.parentElement.remove();
+      input.focus();
     }
-}
 
-add.addEventListener('click',mission);
+    input.focus();
+  }
+};
+
+add.addEventListener("click", mission);
+
+window.onload = () => {
+  input.focus();
+};
+
+input.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    add.click();
+  }
+});
